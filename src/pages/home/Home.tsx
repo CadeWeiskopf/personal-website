10296b7fbcc919c84df8a8387a1d880cf32345b4
Cade Weiskopf <cadew@pm.me>
2023-10-15 15:00:02 -0700

    minor restyle

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,14 @@ import styles from "./Home.module.css";
 const Home: React.FC = () => {
   return (
     <div className={styles.wrapper}>
+      <div className={styles.nameWrapper}>
+        <div className={styles.name}>Cade</div>
+        <div className={styles.name}>Weiskopf</div>
+      </div>
+      <div className={styles.subtitleWrapper}>
+        <div className={styles.subtitle}>&lt;Software Engineer&gt;</div>
+      </div>
+
       <div className={styles.quoteAndAuthorWrapper}>
         <div className={styles.quoteWrapper}>
           <div className={styles.quote}>
@@ -10,14 +18,6 @@ const Home: React.FC = () => {
           </div>
         </div>
         <div className={styles.quoteAuthor}>Saint Augustine</div>
-      </div>
-
-      <div className={styles.nameWrapper}>
-        <div className={styles.name}>Cade</div>
-        <div className={styles.name}>Weiskopf</div>
-      </div>
-      <div className={styles.subtitleWrapper}>
-        <div className={styles.subtitle}>&lt;Software Engineer&gt;</div>
       </div>
     </div>
   );
