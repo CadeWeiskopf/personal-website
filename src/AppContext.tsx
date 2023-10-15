9ddbb485b5caa783a65cb9d8fd03658ec9aed2b7
Cade Weiskopf <cadew@pm.me>
2023-10-15 12:24:42 -0700

    rm comment

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -6,8 +6,6 @@ const AppContext = React.createContext<IAppContext>({});
 
 interface AppContextProviderProps {
   children: ReactNode;
-  // Define any props that you might want to pass in the future
-  // For now, it's an empty interface because there are no specific props.
 }
 
 export const AppContextProvider: React.FC<AppContextProviderProps> = (
