ff3ad9856b775ea508b32a60faea76388dd6eafd
Cade Weiskopf <cadew@pm.me>
2023-10-21 19:32:29 -0700

    usernames + // eslint-disable-next-line react-hooks/exhaustive-deps

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,8 +1,32 @@
+import React from "react";
 import { Client, IMessage } from "./client";
 import styles from "./Chat.module.css";
 import { useContext, useEffect, useRef, useState } from "react";
 import { AppContext } from "../../AppContext";
 import { IMessageEvent } from "websocket";
+
+const GetUsernameComponent: React.FC<{
+  setClientUsername: React.Dispatch<React.SetStateAction<string>>;
+}> = ({ setClientUsername }) => {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  return (
+    <div>
+      <form
+        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+          if (!usernameRef.current) {
+            throw Error("missing username ref");
+          }
+          setClientUsername(usernameRef.current.value);
+        }}
+      >
+        <label>Username</label>
+        <input ref={usernameRef} />
+        <button type="submit">ok</button>
+      </form>
+    </div>
+  );
+};
 
 const Chat: React.FC = () => {
   const { setShowHeader } = useContext(AppContext);
@@ -10,6 +34,7 @@ const Chat: React.FC = () => {
     setShowHeader(true);
   }, [setShowHeader]);
 
+  const [clientUsername, setClientUsername] = useState("");
   const [messages, setMessages] = useState<IMessage[]>([]);
   const [client, setClient] = useState<Client>();
 
@@ -17,28 +42,39 @@ const Chat: React.FC = () => {
   const messageInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
+    if (!client) {
+      return;
+    }
     if (!messagesWrapperRef.current) {
       throw Error("missing messagesWrapperRef.current");
     }
     messagesWrapperRef.current.scrollTop =
       messagesWrapperRef.current.scrollHeight;
-  }, [messages]);
+  }, [messages, client]);
 
   useEffect(() => {
+    if (!clientUsername) {
+      return;
+    }
+    if (client) {
+      return;
+    }
+
     const clientOnMessageEvent = (message: IMessageEvent) => {
       setMessages((messages) => {
         messages.push(JSON.parse(message.data.toString()));
         return messages.map((e) => e);
       });
     };
-    const newClient = new Client(
-      `Test${new Date().getTime()}`,
-      clientOnMessageEvent
-    );
+    const newClient = new Client(clientUsername, clientOnMessageEvent);
     setClient(newClient);
+  }, [client, clientUsername]);
+
+  useEffect(() => {
     return () => {
-      newClient.closeConnection();
+      client?.closeConnection();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const sendMessageSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -55,30 +91,36 @@ const Chat: React.FC = () => {
   return (
     <div className={`main-wrapper-with-header ${styles.wrapper}`}>
       <h1>Chat</h1>
-      <p>Hello, welcome</p>
-      <div className={styles.chatWrapper}>
-        <div
-          className={styles.chatMessages}
-          ref={messagesWrapperRef}
-        >
-          {messages.map((message, index) => {
-            return (
-              <div
-                key={`chat-message-${index}`}
-                className={styles.chatMessage}
-              >
-                {message.clientId} -- {message.message}
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.chatInput}>
-          <form onSubmit={sendMessageSubmit}>
-            <input ref={messageInputRef} />
-            <button type="submit">send</button>
-          </form>
-        </div>
-      </div>
+      {!clientUsername ? (
+        <GetUsernameComponent setClientUsername={setClientUsername} />
+      ) : (
+        <>
+          <p>Hello, welcome</p>
+          <div className={styles.chatWrapper}>
+            <div
+              className={styles.chatMessages}
+              ref={messagesWrapperRef}
+            >
+              {messages.map((message, index) => {
+                return (
+                  <div
+                    key={`chat-message-${index}`}
+                    className={styles.chatMessage}
+                  >
+                    {message.clientId} -- {message.message}
+                  </div>
+                );
+              })}
+            </div>
+            <div className={styles.chatInput}>
+              <form onSubmit={sendMessageSubmit}>
+                <input ref={messageInputRef} />
+                <button type="submit">send</button>
+              </form>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
