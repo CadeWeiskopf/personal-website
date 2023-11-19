9ab05b4b86717b58bbea8154022a4ab16904f6a0
Cade Weiskopf <cadew@pm.me>
2023-11-19 00:19:47 -0500

    shared-types updates

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,12 +1,12 @@
 import { useContext, useEffect, useRef } from "react";
 import { AppContext } from "../../AppContext";
 import styles from "./Contact.module.css";
-import { EmailData } from "../../data/shared-types/types";
-
-type DataRef = {
-  ref: React.RefObject<any>;
-  refName: string;
-};
+import { EmailData, isEmailData } from "../../data/shared-types/types";
+import {
+  DataRef,
+  generateObject,
+  newRef,
+} from "../../data/shared-types/data-refs";
 
 const Contact: React.FC = () => {
   const { setShowHeader } = useContext(AppContext);
@@ -14,56 +14,16 @@ const Contact: React.FC = () => {
     setShowHeader(true);
   }, [setShowHeader]);
 
-  const dataRefs: DataRef[] = [
-    {
-      ref: useRef<HTMLInputElement>(null),
-      refName: "firstName",
-    },
-    {
-      ref: useRef<HTMLInputElement>(null),
-      refName: "lastName",
-    },
-    {
-      ref: useRef<HTMLInputElement>(null),
-      refName: "companyName",
-    },
-    {
-      ref: useRef<HTMLInputElement>(null),
-      refName: "email",
-    },
-    {
-      ref: useRef<HTMLInputElement>(null),
-      refName: "phone",
-    },
-  ];
-  const getDataRef = (refName: string): DataRef => {
-    const dataRef = dataRefs.find((e) => e.refName === refName);
-    if (!dataRef) {
-      throw Error(`No dataRef in dataRefs with refName=${refName}`);
-    }
-    return dataRef;
-  };
-  const checkRefCurrent = (dataRef: DataRef) => {
-    if (!!dataRef.ref.current) {
-      return true;
-    }
-    throw Error(`Missing ref: ${dataRef.refName}`);
-  };
+  const dataRefs: DataRef[] = [];
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (!process.env.REACT_APP_EMAIL_SERVICE_URL) {
       throw Error("missing env var REACT_APP_EMAIL_SERVICE_URL");
     }
-    dataRefs.forEach((e) => checkRefCurrent(e));
 
-    const body: EmailData = {
-      firstName: getDataRef("firstName").ref.current.value,
-      lastName: getDataRef("lastName").ref.current.value,
-      companyName: getDataRef("companyName").ref.current.value,
-      email: getDataRef("email").ref.current.value,
-      phone: getDataRef("phone").ref.current.value,
-    };
+    const body: EmailData = generateObject(isEmailData, dataRefs);
+
     const response = await fetch(
       `${process.env.REACT_APP_EMAIL_SERVICE_URL}/email`,
       {
@@ -87,20 +47,24 @@ const Contact: React.FC = () => {
         <h2>Fill out details</h2>
 
         <input
-          ref={getDataRef("firstName").ref}
+          ref={newRef(dataRefs, useRef<HTMLInputElement>(null), "firstName")}
           required
         />
         <input
-          ref={getDataRef("lastName").ref}
+          ref={newRef(dataRefs, useRef<HTMLInputElement>(null), "lastName")}
           required
         />
-        <input ref={getDataRef("companyName").ref} />
         <input
-          ref={getDataRef("email").ref}
+          ref={newRef(dataRefs, useRef<HTMLInputElement>(null), "companyName")}
+        />
+        <input
+          ref={newRef(dataRefs, useRef<HTMLInputElement>(null), "email")}
           required
           type="email"
         />
-        <input ref={getDataRef("phone").ref} />
+        <input
+          ref={newRef(dataRefs, useRef<HTMLInputElement>(null), "phone")}
+        />
 
         <button type="submit">Submit</button>
       </form>
